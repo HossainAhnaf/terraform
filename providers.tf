5253466e65c570189e16963eed71e9ceed2f9ea6
Your Name <you@example.com>
2025-07-06 10:51:04 +0600

    backup

--- providers.tf
+++ providers.tf
@@ -4,11 +4,7 @@ terraform {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "4.33.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.7.2"
-    }
+    } 
   }
   required_version = " >= 1.10.0"
   
