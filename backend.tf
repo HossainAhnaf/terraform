507e0aa61a82e6ebb477b51c31231d7ec291302d
Your Name <you@example.com>
2025-07-06 08:55:38 +0600

    backup

--- backend.tf
+++ backend.tf
@@ -2,9 +2,16 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.80.0"
+      version = "4.33.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.7.2"
     }
   }
+  required_version = " >= 1.10.0"
+}
+
   backend "azurerm" {
     resource_group_name  = "terraform"
     storage_account_name = "tfstatehasan"
@@ -13,4 +20,3 @@ terraform {
     subscription_id      = "fc4b0a21-cf38-4354-9ee7-f3532dd03c57"
   
   }
-}
