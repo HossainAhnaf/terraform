a5ce7faa9c447bec9eb619f21f2201c27586f2ac
Your Name <you@example.com>
2025-07-12 08:22:44 +0600

    backup

--- backend.tf
+++ backend.tf
@@ -4,6 +4,6 @@ terraform {
     resource_group_name  = "terraform"
     storage_account_name = "tfstatehasan"
     container_name       = "tfstate01"
-    key                  = "${local.project_name}.terraform.tfstate"
+    key                  = "spring-react-devops.terraform.tfstate"
   }
 }
--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@ output "database_name" {
   value       = module.database.database_name
   description = "The name of the database"
 }
+
+output "project_name" {
+  value = local.project_name
+}
